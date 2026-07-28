9d9225bc6f0ebad075f0da5eaaf2728c81399bf2
Tanish Aggarwal <atanish6@gmail.com>
2026-07-28 14:37:44 -0400

    Initial Commit

---
+++ TripFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripFlow
+//
+//  Created by Tanish Aggarwal on 2026-07-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
